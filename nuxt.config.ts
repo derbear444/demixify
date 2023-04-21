eac536573f1b12afd00e97c4a6ab96985e0b5f86
Derek Wilson <dcwilson0901@gmail.com>
2023-04-21 14:35:06 -0400

    Added loading stuff from .env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        title: 'Demixify',
+        // Public keys that are exposed to the client
+        public: {
+            apiBase: `${process.env.NUXT_PUBLIC_API_BASE_URL}/api`
+        },
+    },
+    modules: [    
+        '@nuxtjs/tailwindcss',    
+        '@nuxt/image-edge',  
+    ],
 })
