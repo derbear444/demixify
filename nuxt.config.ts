a1b4995c4057848d3fb227e2e9ec6c68680b5088
Derek Wilson <Mdcwilson0901@gmail.com>
2023-05-01 18:25:40 -0400

    Changed web port to run on 8000

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    devServer: {
+        port: 8000
+    },
     runtimeConfig: {
         title: 'Demixify',
         // Public keys that are exposed to the client
