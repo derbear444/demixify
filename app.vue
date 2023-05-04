4eb61a52b379e7e20cf431a43e895f77bb54b750
Derek Wilson <Mdcwilson0901@gmail.com>
2023-05-03 20:43:38 -0400

    Attempting to work a little on presentation

--- app.vue
+++ app.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <textarea v-model="text"></textarea>
-      <button @click="handleSendData">Send Data</button>
-      <MultitrackAudioPlayer v-if="names.length != 0" :songTitle="text" :sources="audios" :source_names="names" />
-    </NuxtLayout>
+  <div :class="themeString" class="h-screen bg-themeBackground p-5">
+    <div class="header flex justify-between items-center">
+      <h1 class="text-themeText font-bold text-6xl tracking-tight leading-tight pb-3">Deximify</h1>
+      <button class="bg-blue-500 hover:bg-blue-600 font-bold py-2 px-4 rounded" @click="toggleDarkMode">
+        {{ darkMode ? 'Light Mode' : 'Dark Mode' }}
+      </button>
+    </div>
+    <div :class="themeString" class="bg-gray-800 shadow-lg rounded-lg p-6">
+      <form @submit.prevent="handleSendData">
+        <div class="flex flex-col space-y-4">
+          <input class="bg-gray-800 border border-gray-300 rounded-lg p-4 text-white" type="text" v-model="text"
+            @keydown.enter="submit" placeholder="Enter text here">
+          <button class="bg-blue-500 hover:bg-blue-600 font-bold py-2 px-4 rounded" type="submit">Send
+            Data</button>
+          <div v-if="loading" class="flex items-center space-x-2">
+            <div class="animate-pulse w-4 h-4 bg-blue-500 rounded-full"></div>
+            <div class="text-white">Loading audio...</div>
+          </div>
+          <MultitrackAudioPlayer v-if="showPlayer" :songTitle="text" :sources="audios" :source_names="names" />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -17,10 +33,13 @@ export default {
       text: '',
       names: [],
       audios: [],
+      loading: false,
+      darkMode: true,
     };
   },
   methods: {
     async handleSendData() {
+      this.loading = true;
       const { data, pending, error, refresh } = await useFetch(
         `http://152.10.212.186:5000/api/combos`,
         {
@@ -47,7 +66,7 @@ export default {
       const check = combos.includes(comboString);
 
       if (check) {
-        this.sendLimitedData(this.text).then((data) => {
+        await this.sendLimitedData(this.text).then((data) => {
           if (typeof data != "string") {
             this.names = data.map((source) => source.name);
             this.audios = data.map((source) => source.audio);
@@ -62,6 +81,7 @@ export default {
           }
         });
       }
+      this.loading = false;
     },
     async sendData(song_name) {
       console.log("Attempting to load brand new song...")
@@ -116,6 +136,17 @@ export default {
       );
       const sourcesArr = JSON.parse(data.value);
       return sourcesArr;
+    },
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+    },
+  },
+  computed: {
+    showPlayer() {
+      return this.names.length !== 0
+    },
+    themeString() {
+      return this.darkMode ? "theme-dark" : "theme-light"
     },
   },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        themeBackground: 'var(--background)',
+        themeText: 'var(--text)',
+      },
+    },
+  },
+  plugins: [],
+}
+
